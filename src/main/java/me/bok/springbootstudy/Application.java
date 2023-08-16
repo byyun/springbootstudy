c9622b7e88424a392d018b7d1cbd861826c69e74
윤복영 <phantom@jobkorea.co.kr>
2023-08-16 21:24:34 +0900

    내장 톰캣

--- src/main/java/me/bok/springbootstudy/Application.java
+++ src/main/java/me/bok/springbootstudy/Application.java
@@ -1,5 +1,14 @@
 package me.bok.springbootstudy;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
@@ -17,10 +26,36 @@ import me.bok.Holoman;
 public class Application {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
+		/*
 		SpringApplication application = new SpringApplication(Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
+		*/
+		/*
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		tomcat.getConnector();
+		Context context = tomcat.addContext("/", "/");
+		HttpServlet servlet = new HttpServlet() {
+			@Override
+			protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+				PrintWriter writer = resp.getWriter();
+				writer.println("<html><head><title>");
+				writer.println("Hello,tomcat");
+				writer.println("</title></head>");
+				writer.println("<body><h1>Hello, world</h1></body>");
+				writer.println("</html>");
+			}
+		};
+
+		String servletName = "helloServlet";
+		tomcat.addServlet("/",servletName, servlet);
+		context.addServletMappingDecoded("/hello", servletName);
+		tomcat.start();
+		tomcat.getServer().await();
+
+		 */
 	}
 
 	/*
